bfb3e87d7c52c4b747b3dca55dcf7f282dd56771
He Wang <wanghechn@qq.com>
2021-10-24 11:40:38 +0800

    cleanup exception class

--- logproxy-client/src/main/java/com/oceanbase/clogproxy/client/exception/LogProxyClientException.java
+++ logproxy-client/src/main/java/com/oceanbase/clogproxy/client/exception/LogProxyClientException.java
@@ -12,31 +12,67 @@ package com.oceanbase.clogproxy.client.exception;
 
 import com.oceanbase.clogproxy.client.enums.ErrorCode;
 
+/**
+ * This is a subclasses of {@link RuntimeException} used to indicate the exception occurs in client with an error code.
+ */
 public class LogProxyClientException extends RuntimeException {
+
+    /**
+     * Error code.
+     */
     private ErrorCode code = ErrorCode.NONE;
 
+    /**
+     * Constructor with error code and message.
+     *
+     * @param code    Error code.
+     * @param message Error message.
+     */
     public LogProxyClientException(ErrorCode code, String message) {
         super(message);
         this.code = code;
     }
 
+    /**
+     * Constructor with error code and exception.
+     *
+     * @param code      Error code.
+     * @param exception Exception instance.
+     */
     public LogProxyClientException(ErrorCode code, Exception exception) {
         super(exception.getMessage(), exception.getCause());
         this.code = code;
     }
 
+    /**
+     * Constructor with error code, error message and cause.
+     *
+     * @param code      Error code.
+     * @param message   Error message.
+     * @param throwable Cause.
+     */
     public LogProxyClientException(ErrorCode code, String message, Throwable throwable) {
         super(message, throwable);
         this.code = code;
     }
 
+    /**
+     * Identify whether the client should stop the stream.
+     *
+     * @return The flag of whether the client should stop the stream.
+     */
     public boolean needStop() {
         return (code == ErrorCode.E_MAX_RECONNECT) || (code == ErrorCode.E_PROTOCOL) ||
-                (code == ErrorCode.E_HEADER_TYPE) || (code == ErrorCode.NO_AUTH) ||
-                (code == ErrorCode.E_COMPRESS_TYPE) || (code == ErrorCode.E_LEN) ||
-                (code == ErrorCode.E_PARSE);
+            (code == ErrorCode.E_HEADER_TYPE) || (code == ErrorCode.NO_AUTH) ||
+            (code == ErrorCode.E_COMPRESS_TYPE) || (code == ErrorCode.E_LEN) ||
+            (code == ErrorCode.E_PARSE);
     }
 
+    /**
+     * Get the error code.
+     *
+     * @return Error code.
+     */
     public ErrorCode getCode() {
         return code;
     }
