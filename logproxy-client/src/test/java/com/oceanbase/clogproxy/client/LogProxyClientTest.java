cd81de1486fdfda1f66af681eece63dbab0fc557
He Wang <wanghechn@qq.com>
2022-07-11 20:15:20 +0800

    update test with more detailed example

--- logproxy-client/src/test/java/com/oceanbase/clogproxy/client/LogProxyClientTest.java
+++ logproxy-client/src/test/java/com/oceanbase/clogproxy/client/LogProxyClientTest.java
@@ -15,10 +15,9 @@ import com.oceanbase.clogproxy.client.config.ClientConf;
 import com.oceanbase.clogproxy.client.config.ObReaderConfig;
 import com.oceanbase.clogproxy.client.exception.LogProxyClientException;
 import com.oceanbase.clogproxy.client.listener.RecordListener;
+import com.oceanbase.oms.logmessage.DataMessage;
 import com.oceanbase.oms.logmessage.LogMessage;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import javax.net.ssl.SSLException;
+import java.util.stream.Collectors;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -31,42 +30,38 @@ public class LogProxyClientTest {
 
     @Test
     public void testLogProxyClient() {
+        // Can get it with "show parameters like 'obconfig_url'"
+        String clusterUrl =
+                "http://127.0.0.1:8080/services"
+                        + "?Action=ObRootServiceInfo"
+                        + "&User_ID=alibaba"
+                        + "&UID=ocpmaster"
+                        + "&ObRegion=obcluster";
+
+        // Can get it with "show parameters like 'rootservice_list'"
+        String rsList = "127.0.0.1:2882:2881";
+
         ObReaderConfig config = new ObReaderConfig();
-        config.setRsList("127.0.0.1:2882:2881");
-        config.setUsername("root@sys");
-        config.setPassword("root@sys");
+
+        // Either 'rsList' or 'clusterUrl' should be not empty, will try to use 'clusterUrl' firstly
+        config.setRsList(rsList);
+        // config.setClusterUrl(clusterUrl);
+        config.setUsername("root@test_tenant");
+        config.setPassword("pswd");
         config.setStartTimestamp(0L);
-        config.setTableWhiteList("sys.test.*");
+        config.setTableWhiteList("test_tenant.test_db.*");
+        config.setTableBlackList("test_tenant.test_db.test_table");
+        config.setTimezone("+8:00");
+        config.setWorkingMode("memory");
 
-        LogProxyClient client = new LogProxyClient("127.0.0.1", 2983, config);
-
-        client.addListener(
-                new RecordListener() {
-
-                    @Override
-                    public void notify(LogMessage message) {
-                        logger.info("LogMessage received: {}", message.toString());
-                    }
-
-                    @Override
-                    public void onException(LogProxyClientException e) {
-                        logger.error(e.getMessage());
-                    }
-                });
-        client.start();
-        client.join();
-    }
-
-    @Test
-    public void testLogProxyClientWithSsl() throws SSLException {
-        ObReaderConfig config = new ObReaderConfig();
-        config.setRsList("127.0.0.1:2882:2881");
-        config.setUsername("root@sys");
-        config.setPassword("root@sys");
-        config.setStartTimestamp(0L);
-        config.setTableWhiteList("sys.test.*");
-
-        ClientConf clientConf = ClientConf.builder().sslContext(sslContext()).build();
+        ClientConf clientConf =
+                ClientConf.builder()
+                        .transferQueueSize(1000)
+                        .connectTimeoutMs(3000)
+                        .maxReconnectTimes(100)
+                        .ignoreUnknownRecordType(true)
+                        .clientId("test")
+                        .build();
 
         LogProxyClient client = new LogProxyClient("127.0.0.1", 2983, config, clientConf);
 
@@ -75,7 +70,48 @@ public class LogProxyClientTest {
 
                     @Override
                     public void notify(LogMessage message) {
-                        logger.info("LogMessage received: {}", message.toString());
+                        switch (message.getOpt()) {
+                            case INSERT:
+                            case UPDATE:
+                            case DELETE:
+                                // note that the db name contains prefix '{tenant}.'
+                                logger.info(
+                                        "Received log message of type {}: db: {}, table: {}, checkpoint {}",
+                                        message.getOpt(),
+                                        message.getDbName(),
+                                        message.getTableName(),
+                                        message.getCheckpoint());
+                                // old fields for type 'UPDATE', 'DELETE'
+                                logger.info(
+                                        "Old field values: {}",
+                                        message.getFieldList().stream()
+                                                .filter(DataMessage.Record.Field::isPrev)
+                                                .collect(Collectors.toList()));
+                                // new fields for type 'UPDATE', 'INSERT'
+                                logger.info(
+                                        "New field values: {}",
+                                        message.getFieldList().stream()
+                                                .filter(field -> !field.isPrev())
+                                                .collect(Collectors.toList()));
+                                break;
+                            case HEARTBEAT:
+                                logger.info(
+                                        "Received heartbeat message with checkpoint {}",
+                                        message.getCheckpoint());
+                                break;
+                            case BEGIN:
+                            case COMMIT:
+                                logger.info("Received transaction message {}", message.getOpt());
+                                break;
+                            case DDL:
+                                logger.info(
+                                        "Received log message with DDL: {}",
+                                        message.getFieldList().get(0).getValue().toString());
+                                break;
+                            default:
+                                throw new IllegalArgumentException(
+                                        "Unsupported log message type: " + message.getOpt());
+                        }
                     }
 
                     @Override
@@ -85,15 +121,5 @@ public class LogProxyClientTest {
                 });
         client.start();
         client.join();
-    }
-
-    private SslContext sslContext() throws SSLException {
-        return SslContextBuilder.forClient()
-                .sslProvider(SslContext.defaultClientProvider())
-                .trustManager(this.getClass().getClassLoader().getResourceAsStream("certs/ca.crt"))
-                .keyManager(
-                        this.getClass().getClassLoader().getResourceAsStream("certs/client.crt"),
-                        this.getClass().getClassLoader().getResourceAsStream("certs/client.key"))
-                .build();
     }
 }
