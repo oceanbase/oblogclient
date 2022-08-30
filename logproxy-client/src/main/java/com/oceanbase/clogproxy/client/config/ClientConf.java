37603aa15a19c8de87005afbba9f61ee5f7bbebd
He Wang <wanghechn@qq.com>
2022-08-30 17:22:58 +0800

    update version and error log message (#54)
    update version and log message

--- logproxy-client/src/main/java/com/oceanbase/clogproxy/client/config/ClientConf.java
+++ logproxy-client/src/main/java/com/oceanbase/clogproxy/client/config/ClientConf.java
@@ -22,7 +22,7 @@ public class ClientConf extends SharedConf implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /** Client version. */
-    public static final String VERSION = "1.0.5";
+    public static final String VERSION = "1.0.7";
 
     /** Queue size for storing records received from log proxy. */
     private final int transferQueueSize;
